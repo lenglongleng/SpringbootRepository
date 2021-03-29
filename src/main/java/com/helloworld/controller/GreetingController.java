f1426ee3ced93b5776daef305bb8cadfcc624757
lenglongleng <lenglongleng@gmail.com>
2021-03-29 16:37:51 +0700

    add controller

---
+++ src/main/java/com/helloworld/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.helloworld.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api")
+public class GreetingController {
+
+	@RequestMapping(value="/student/get", method = RequestMethod.GET)
+	public String checkAndStoreCustomerData() {
+		
+		System.out.println("Returning data from server");
+		//Thread.sleep(10*1000);
+		return "Hello from Server";
+	}
+	
+	/*
+	@RequestMapping(value="/insert/student", method = RequestMethod.POST)
+	public ResponseEntity<CheckAndStoreCustomerDataResponse> checkAndStoreCustomerData(@RequestBody CheckAndStoreCustomerDataRequest request) {
+		
+	}
+	*/
+}

f35cafdc8a69995987aaf0b5a92de85680984024
lenglongleng <lenglongleng@gmail.com>
2021-03-29 16:15:43 +0700

    first commit

---
+++ src/main/java/com/helloworld/SpringBootRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/SpringBootRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

27e1086cb3626a9df4158b3a427e31a6957e43ef
lenglongleng <lenglongleng@gmail.com>
2021-03-29 16:38:21 +0700

    update commit

--- src/main/java/com/helloworld/controller/GreetingController.java
+++ src/main/java/com/helloworld/controller/GreetingController.java
@@ -1,7 +1,5 @@
 package com.helloworld.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
